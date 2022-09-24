9ef979c1cfff97230607678fc4331c4717071a73
hikari <tanimoto0709@gmail.com>
2022-09-24 22:24:25 +0900

    update: todolistの追加処理

--- src/script/common.ts
+++ src/script/common.ts
@@ -1,8 +1,21 @@
 // developmentやprod
 const addTodoButton: HTMLElement | null =
   document.getElementById('js-add-todo');
-addTodoButton!.addEventListener('click', hoge);
+const todoList: HTMLElement | null = document.getElementById('js-todo-list');
+//const todoTitle = todoTitleElement?.value;
 
-function hoge() {
-  alert('hoge');
-}
+const appendTodoList = (event: any) => {
+  event.preventDefault();
+  const todoTitleElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById('js-todo-title')
+  );
+  const todoTitle: string = todoTitleElement.value;
+
+  const listItem = `
+  <li class="border-bottom border-solid border-gray-100 p-2">${todoTitle}</li>
+`;
+  todoList?.insertAdjacentHTML('afterbegin', listItem);
+  todoTitleElement.value = '';
+};
+
+addTodoButton!.addEventListener('click', appendTodoList);
